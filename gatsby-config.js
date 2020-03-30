d904201fdb704e40c7ce8d6590eec0c2c7fdcc74
unknown <oskarkwasniewski@icloud.com>
2020-03-30 17:31:36 +0200

    final changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -40,7 +40,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `standalone`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/okwasniewski-icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
